dea8d89d4f70ac0181a646a6055202b88fa480a2
Athina Kyriakou <athina.skyriakou@gmail.com>
2021-04-28 15:46:33 +0300

    Configured Kafka for local development and created dummy producer-consumer

--- README.md
+++ README.md
@@ -2,16 +2,55 @@
 
 ## Local Development
 
+### Kafka
+
+Run Docker Compose on a terminal
+```bash
+docker-compose up -d
+```
+
+Check that everything is up and running
+```bash
+docker-compose ps
+```
+
+Create topic
+```bash
+docker-compose exec broker kafka-topics \
+  --create \
+  --bootstrap-server localhost:9092 \
+  --replication-factor 1 \
+  --partitions 1 \
+  --topic kdriver
+```
 
 ## Useful Resources
 
 ### Kubernetes
+[Kubectl Cheat Sheet](https://kubernetes.io/docs/reference/kubectl/cheatsheet/)
+
+[How to Access a Kubernetes Cluster](https://kubernetes.io/docs/tasks/access-application-cluster/access-cluster/)
+
+[Cluster Access Configuration](https://kubernetes.io/docs/tasks/access-application-cluster/configure-access-multiple-clusters/)
+
+[Configure kubectl to Access Remote Kubernetes Cluster](https://acloudguru.com/hands-on-labs/configuring-kubectl-to-access-a-remote-cluster)
+
+[Kubeconfig tips with Kubectl](https://ahmet.im/blog/mastering-kubeconfig/)
+
+//todo
+[1](https://kubernetes.io/docs/concepts/workloads/controllers/deployment/)
+[2](https://www.mirantis.com/blog/introduction-to-yaml-creating-a-kubernetes-deployment/)
+[3](https://kubernetes.io/docs/concepts/overview/working-with-objects/)
+
+### Kubernetes & Python
 [Python Client](https://github.com/kubernetes-client/python)
+
 [Python Client API Endpoints](https://github.com/kubernetes-client/python/blob/master/kubernetes/README.md#documentation-for-api-endpoints)
 
 [Get Started Kubernetes with Python](https://kubernetes.io/blog/2019/07/23/get-started-with-kubernetes-using-python/)
-[Kubectl Cheat Sheet](https://kubernetes.io/docs/reference/kubectl/cheatsheet/)
-[How to Access a Kubernetes Cluster](https://kubernetes.io/docs/tasks/access-application-cluster/access-cluster/)
-[Cluster Access Configuration](https://kubernetes.io/docs/tasks/access-application-cluster/configure-access-multiple-clusters/)
-[Configure kubectl to Access Remote Kubernetes Cluster](https://acloudguru.com/hands-on-labs/configuring-kubectl-to-access-a-remote-cluster)
-[Kubeconfig tips with Kubectl](https://ahmet.im/blog/mastering-kubeconfig/)
+
+### Kafka
+[Kafka CheatSheet](https://docs.confluent.io/platform/current/quickstart/cos-docker-quickstart.html)
+
+### Kafka & Python
+[Getting Started with Apache Kafka in Python](https://towardsdatascience.com/getting-started-with-apache-kafka-in-python-604b3250aa05)
--- drivers/kubernetes.py
+++ drivers/kubernetes.py
@@ -1,33 +1,81 @@
 from os import path
 from pprint import pprint
 import yaml
+from confluent_kafka import Consumer, KafkaException, KafkaError
 from kubernetes import client, config
 from kubernetes.client import Configuration
 from kubernetes.config import kube_config
 
-class KubernetesDriver(object):
+SUCCESS = 'SUCCESS'
+FAILURE = 'FAILURE'
+
+class K8sDriver(object):
     
-    def __init__(self, kubeconfig_yaml):
+    def __init__(self, kubeconfig_yaml, kafka_conf, consumer_topics):
         self.kubeconfig_yaml = kubeconfig_yaml
         self._kubeconfig_yaml = None
-        
-        # configuration setup
-        k8_loader = kube_config.KubeConfigLoader(self.config)
-        call_config = type.__call__(Configuration)
-        k8_loader.load_and_set(call_config)
-        Configuration.set_default(call_config)
+
+        # Kubernetes connector configuration setup
+        self.k8s_connect = self.k8s_config
+        print("Kubernetes connector configuration:", self.k8s_connect)
+
+        #Kafka consumer loop
+        #consumer not thread safe
+        if(self.k8s_connect == SUCCESS):
+            self.consumer = Consumer(kafka_conf)
+            self.consumer_topics = consumer_topics
+            self.consume_loop
+
 
     @property
-    def config(self):
-        with open(self.kubeconfig_yaml, 'r') as f:
-            if self._kubeconfig_yaml is None:
-                self._kubeconfig_yaml = yaml.safe_load(f)
-        return self._kubeconfig_yaml
+    def k8s_config(self):
+        try:
+            with open(self.kubeconfig_yaml, 'r') as (f):
+                if self._kubeconfig_yaml is None:
+                    self._kubeconfig_yaml = yaml.safe_load(f)
+                try: 
+                    k8_loader = kube_config.KubeConfigLoader(self._kubeconfig_yaml)
+                    call_config = type.__call__(Configuration)
+                    k8_loader.load_and_set(call_config)
+                    Configuration.set_default(call_config)
+                    return(SUCCESS)
+                except client.exceptions.ApiException as e:
+                    print("Failure to connect to the Kubernetes cluster: %s" % e)
+                    return(FAILURE)
+        except IOError as err:
+            print("IOError:", err)
+            return(FAILURE)
+
+    @property
+    #dummy consuming loop
+    def consume_loop(self):
+        try:
+            self.consumer.subscribe(self.consumer_topics)
+            while True:
+                #print("Waiting for a message from kdriver topic...")
+                msg = self.consumer.poll(timeout=1.0)
+                if msg is None: continue
+                if msg.error():
+                    if msg.error().code() == KafkaError._PARTITION_EOF:
+                        # End of partition event
+                        sys.stderr.write('%% %s [%d] reached end at offset %d\n' %
+                                        (msg.topic(), msg.partition(), msg.offset()))
+                    elif msg.error():
+                        raise KafkaException(msg.error())
+                else:
+                    #msg_process(msg)
+                    print(msg)
+        except KafkaException as e:
+            print("Failure to initialize Kafka consumer: %s" % e)
+        finally:
+            # Close down consumer to commit final offsets.
+            self.consumer.close()
 
     def get_pods(self, namespace):
         core_v1 = client.CoreV1Api()
-        pods = core_v1.list_pod_for_all_namespaces(watch=False)
-        for i in pods.items:
+        api_response = core_v1.list_pod_for_all_namespaces(watch=False)
+        #pprint(api_response)
+        for i in api_response.items:
             if(i.metadata.namespace == namespace):
                 print('%s\t%s\t%s' % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
     
@@ -38,7 +86,7 @@ class KubernetesDriver(object):
             apps_v1 = client.AppsV1Api()
             try:
                 api_response = apps_v1.create_namespaced_deployment(body=dep, namespace=namespace)
-                pprint(api_response)
-                #print("Deployment created. status='%s'" % resp.metadata.name)
+                #pprint(api_response)
+                print("Deployment created. status='%s'" % api_response.metadata.name)
             except client.exceptions.ApiException as e:
                 print("Deployment exception: %s" % e)
